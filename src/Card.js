0eef917627261776fc5c6ed5699cfad56e5e6084
Claire <claire.potter.shen@gmail.com>
2022-09-18 16:34:15 -0400

    test

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
     return (
-        <div className = 'bg-light-pink dib br2 pa3 ma2 grow bw2 shadow-6'>
+        <div className = 'bg-light-pink dib br2 pa3 ma3 grow bw2 shadow-6'>
             <img alt ='profile' src={`https://robohash.org/${id}?200x200`}/>
         <div>
             <h2>{name}</h2>
