7768b55bebd3693faab592561dac4c39f7fae9e2
Claire <claire.potter.shen@gmail.com>
2023-01-02 08:54:10 -0500

    API image change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import { render } from '@testing-library/react';
 
 
-const App = () => {
-    return (
-        <div className = 'tc'>
-            <h1> Gym Friends</h1>
-            <SearchBox />
-            <CardList robots = {robots}/>
-        </div>
-    );
-}
+class App extends Component {
 
-export default App;
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value})
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+
+        return (
+            <div className = 'tc'>
+            <h1> Gym Friends </h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots = {filteredRobots}/>
+            </div>
+            );
+        }
+    }
+    
+ export default App;
--- src/Card.js
+++ src/Card.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
     return (
-        <div className = 'bg-light-pink dib br2 pa3 ma3 grow bw2 shadow-6'>
-            <img alt ='profile' src={`https://robohash.org/${id}?200x200`}/>
+        <div className = 'bg-light-blue dib br4 pa3 ma3 grow bw2 shadow-6'>
+            <img alt ='profile' src={`https://avatars.dicebear.com/api/croodles/${id}:1.png`}/>
         <div>
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
         </div>
-    );
+    )
 }
 
 export default Card; 
